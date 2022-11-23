270bb668bcc6ed5cd56ef9350fb3636aa6b35e64
Yuriy Kryuchkov <ysilber175@gmail.com>
2022-11-23 09:46:46 +0300

    task java 2

--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,22 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args)  {
-        
-        System.out.println("Введите текст и нажмите <Enter>:");
-        String text = new Scanner(System.in).nextLine();
-        System.out.println("Длина текста: " + text.length());
+        int  firstNumber = 4;
+         int secondNumber = 5;
+         double quotient = (double) firstNumber/secondNumber;
+        System.out.println("Введите первое число и нажмите <Enter>:");
+        firstNumber = new Scanner(System.in).nextInt();
+        System.out.println("Введите второе число  и нажмите <Enter>:");
+        secondNumber = new Scanner(System.in).nextInt();
 
+
+        quotient = (double) firstNumber+secondNumber;
+        System.out.println("Сумма : " + quotient);
+        quotient = (double) firstNumber-secondNumber;
+        System.out.println("Разность : " + quotient);
+        quotient = (double) firstNumber*secondNumber;
+        System.out.println("Произведение : " + quotient);
+        quotient = (double) firstNumber/secondNumber;
+        System.out.println("Частное : " + quotient);
     }
 }
