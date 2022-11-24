9ac4772deb6ce99abb4019ef3020412198bf697e
Yuriy Kryuchkov <ysilber175@gmail.com>
2022-11-24 23:13:24 +0300

    hotfix

--- src/Main.java
+++ src/Main.java
@@ -2,22 +2,19 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args)  {
-        int  firstNumber = 4;
-         int secondNumber = 5;
-         double quotient = (double) firstNumber/secondNumber;
+
         System.out.println("Введите первое число и нажмите <Enter>:");
-        firstNumber = new Scanner(System.in).nextInt();
+        int firstNumber = new Scanner(System.in).nextInt();
         System.out.println("Введите второе число  и нажмите <Enter>:");
-        secondNumber = new Scanner(System.in).nextInt();
+        int secondNumber = new Scanner(System.in).nextInt();
 
-
-        quotient = (double) firstNumber+secondNumber;
-        System.out.println("Сумма : " + quotient);
-        quotient = (double) firstNumber-secondNumber;
-        System.out.println("Разность : " + quotient);
-        quotient = (double) firstNumber*secondNumber;
-        System.out.println("Произведение : " + quotient);
-        quotient = (double) firstNumber/secondNumber;
+        int VResult =  firstNumber+secondNumber;
+        System.out.println("Сумма : " + VResult);
+        VResult =  firstNumber-secondNumber;
+        System.out.println("Разность : " + VResult);
+        VResult =  firstNumber*secondNumber;
+        System.out.println("Произведение : " + VResult);
+        double quotient = (double) firstNumber/secondNumber;
         System.out.println("Частное : " + quotient);
     }
 }
