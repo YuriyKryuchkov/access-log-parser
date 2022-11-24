90a8f80ad01c1cf0e7f0563b366e6fee96da5694
Yuriy Kryuchkov <ysilber175@gmail.com>
2022-11-24 16:52:15 +0300

    hotfix2

--- src/Main.java
+++ src/Main.java
@@ -4,5 +4,7 @@ public class Main {
     public static void main(String[] args)  {
         System.out.println("Введите текст и нажмите <Enter>:");
         String text = new Scanner(System.in).nextLine();
-        System.out.println("Длина текста: " + text.length());    }
+        System.out.println("Длина текста: " + text.length());
+
+    }
 }
